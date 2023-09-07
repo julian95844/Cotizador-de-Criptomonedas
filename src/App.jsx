854d628b69637498cddc22644b419f44e07c0052
julian95844 <julian95844@hotmail.com>
2023-09-06 23:14:58 -0300

    feat: añadiendo un spinner y finalizando proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import styled from "@emotion/styled";
 import Formulario from "./componentes/Formulario";
 import Resultado from "./componentes/Resultado";
+import Spinner from "./componentes/Spinner";
 import ImagenCripto from "./img/Imagen-criptos.png";
 
 const Contenedor = styled.div`
@@ -42,11 +43,13 @@ function App() {
 
   const [monedas, setMonedas] = useState ({})
   const [resultado, setResultado] = useState ({})
-
+  const [cargando, setCargando] = useState (false)
 
   useEffect(() =>{
     if(Object.keys(monedas).length > 0){
       const cotizarCripto = async () => {
+        setCargando(true)
+        setResultado({})
         const {moneda, criptomoneda} = monedas
         const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`
 
@@ -54,6 +57,8 @@ function App() {
         const resultado = await respuesta.json()
 
         setResultado(resultado.DISPLAY[criptomoneda][moneda])
+
+        setCargando(false)
       }
 
       cotizarCripto()
@@ -72,6 +77,7 @@ function App() {
         <Formulario 
           setMonedas={setMonedas}
         />
+        {cargando && <Spinner/>}
         {resultado.PRICE && <Resultado resultado={resultado}/>}
       </div>
 
