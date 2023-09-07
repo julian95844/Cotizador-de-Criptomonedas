ef9b11c61785f62664c749395b0cc81979c1cbdf
julian95844 <julian95844@hotmail.com>
2023-09-06 22:03:04 -0300

    feat: validacion de formulario

--- src/Componentes/Formulario.jsx
+++ src/Componentes/Formulario.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState} from 'react'
-import useSelectMonedas from "../hooks/useSelectMonedas";
 import styled from '@emotion/styled'
+import Error from "./Error"
+import useSelectMonedas from "../hooks/useSelectMonedas"
 import { monedas } from "../data/monedas"
 
 
@@ -24,6 +25,8 @@ const InputSubmit = styled.input`
 const Formulario = () => {
 
   const [ criptos, setCriptos ] = useState ([])
+  const [ error, setError ] = useState (false)
+
   const [ moneda, SelectMonedas ] = useSelectMonedas("Eligir tu Moneda", monedas) // no porque se llame de una manera tiene que llamarse asi, en el arreglo te retorna por indice
   const [ criptomoneda, SelectCriptomoneda ] = useSelectMonedas("Eligir tu Criptomoneda", criptos) // no porque se llame de una manera tiene que llamarse asi, en el arreglo te retorna por indice
 
@@ -47,17 +50,31 @@ const Formulario = () => {
       consultarAPI ()
   }, []) 
 
+  const handleSumit = e => {
+    e.preventDefault()
+    if([moneda, criptomoneda].includes("")) {
+      setError(true)
+      return
+    }
+    setError(false)
+  }
+
   return (
-    <form>
+    <>
+      {error && <Error>Todos los campos son obligatorios</Error>}
+      <form
+        onSubmit={handleSumit}
+      >
 
-      < SelectMonedas />
-      <SelectCriptomoneda/>
+        < SelectMonedas />
+        <SelectCriptomoneda/>
 
-      <InputSubmit 
-        type='submit' 
-        value="Cotizar"
-      />
-    </form>
+        <InputSubmit 
+          type='submit' 
+          value="Cotizar"
+        />
+      </form>
+    </>
   )
 }
 
